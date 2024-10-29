f23a2f8ca0cb5d2de9431f04d299050636877315
VadimKorobec <_s_e_t__@ukr.net>
2024-10-29 14:09:42 +0200

    add data.ts and map() inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
+import reactImage from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function getRandomInt(max: number) {
+function genRandomInt(max: number) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
+  const description = reactDescriptions[genRandomInt(1)];
+
   return (
     <>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImage} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactDescriptions[getRandomInt(2)]} React concepts you will need for
-          almost any app you are going to build!
+          {description} React concepts you will need for almost any app you are
+          going to build!
         </p>
       </header>
     </>
+  );
+}
+
+interface CoreConceptProps {
+  title?: string;
+  description?: string;
+  img?: string;
+}
+
+function CoreConcept({ title, description, img }: CoreConceptProps) {
+  return (
+    <div>
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
   );
 }
 
@@ -24,7 +46,20 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item, index) => (
+              <li key={index}>
+                <CoreConcept
+                  title={item.title}
+                  description={item.description}
+                  img={item.image}
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
